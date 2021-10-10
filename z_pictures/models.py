46f80f66020b1cadecd17049825ca25047960707
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-11 00:54:14 +0300

    Added the location model and related methods

--- z_pictures/models.py
+++ z_pictures/models.py
@@ -39,3 +39,23 @@ class Image(models.Model):
     def __str__(self):
         return self.name
 
+
+class Location(models.Model):
+    name = models.CharField(max_length=60)
+
+    @classmethod
+    def get_locations(cls):
+        return Location.objects.all()
+
+    @classmethod
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(name=value)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
