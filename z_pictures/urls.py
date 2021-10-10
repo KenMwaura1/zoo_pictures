b888a546dd8401c14f8ea834f6ba8ebbbc635bad
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-11 01:07:57 +0300

    added initial routes to the project and app urls

---
+++ z_pictures/urls.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('search/', views.search, name='search'),
+    path('location/?p<loc>/', views.location, name='location'),
+
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
