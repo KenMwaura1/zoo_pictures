567ea1955e4bdb33b6531b786cc4eb898eefa86f
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-11 01:07:28 +0300

    added initial routes

--- z_pictures/views.py
+++ z_pictures/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+
+
 # Create your views here.
+def home(request):
+    return render(request, 'all-pictures/home.html')
+
+
+def search(request):
+    return None
+
+
+def location(request):
+    return None
