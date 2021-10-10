b20b84f1a5793b85db26c1b81aadfd24026c23a9
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-11 00:52:24 +0300

    Added the image model and related methods

--- z_pictures/models.py
+++ z_pictures/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
+
 # Create your models here.
+class Image(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.TextField()
+    author = models.CharField(max_length=40, default='admin')
+    image = models.ImageField(upload_to='images/')
+    date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category)
+    location = models.ForeignKey(Location)
+
+    class Meta:
+        ordering = ['date']
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        return cls.objects.filter(id=id).all()
+
+    @classmethod
+    def filter_by_location(cls, location):
+        return Image.objects.filter(location__name=location).all()
+
+    @classmethod
+    def update_image(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+    @classmethod
+    def search_by_category(cls, category):
+        return cls.objects.filter(category__name__icontains=category)
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
+
