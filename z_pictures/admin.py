137c11420222abdd715668fb63df3841d7516233
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-11 01:06:24 +0300

    registered our models in the admin.py

--- z_pictures/admin.py
+++ z_pictures/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+
 # Register your models here.
+from z_pictures.models import Category, Image, Location
+
+admin.site.register(Category)
+admin.site.register(Image)
+admin.site.register(Location)
