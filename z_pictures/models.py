cde21755e003ba0706db8e79e329295e487915f2
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-11 00:58:32 +0300

    Added the category model and related methods

--- z_pictures/models.py
+++ z_pictures/models.py
@@ -1,15 +1,49 @@
 from django.db import models
 
 
+class Location(models.Model):
+    name = models.CharField(max_length=60)
+
+    @classmethod
+    def get_locations(cls):
+        return Location.objects.all()
+
+    @classmethod
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(name=value)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=45)
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
+
+
 # Create your models here.
 class Image(models.Model):
-    name = models.CharField(max_length=60)
+    name = models.CharField(max_length=65)
     description = models.TextField()
-    author = models.CharField(max_length=40, default='admin')
+    author = models.CharField(max_length=43, default='admin')
     image = models.ImageField(upload_to='images/')
     date = models.DateTimeField(auto_now_add=True)
-    category = models.ForeignKey(Category)
-    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
 
     class Meta:
         ordering = ['date']
@@ -34,27 +68,6 @@ class Image(models.Model):
         self.save()
 
     def delete_image(self):
-        self.delete()
-
-    def __str__(self):
-        return self.name
-
-
-class Location(models.Model):
-    name = models.CharField(max_length=60)
-
-    @classmethod
-    def get_locations(cls):
-        return Location.objects.all()
-
-    @classmethod
-    def update_location(cls, id, value):
-        cls.objects.filter(id=id).update(name=value)
-
-    def save_location(self):
-        self.save()
-
-    def delete_location(self):
         self.delete()
 
     def __str__(self):
