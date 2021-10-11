bf8ee7523eaf865eba0d4d527b650a104af56e15
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-11 03:12:12 +0300

    Updated the views to display images by location

--- z_pictures/views.py
+++ z_pictures/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 
+from .models import Category, Image, Location
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'all-pictures/home.html')
+    all_images = Image.objects.all()
+    all_locations = Location.objects.all()
+    all_categories = Category.objects.all()
+    return render(request, 'all-pictures/home.html', {'all_locations': all_locations, 'all_images': all_images,
+                                                      'all_categories': all_categories})
 
 
-def search(request):
+def search(request,):
     return None
 
 
-def location(request):
-    return None
+def location(request, location):
+    all_images = Image.filter_by_location(location)
+    return render(request, 'all-pictures/location.html', {'all_images': all_images})
